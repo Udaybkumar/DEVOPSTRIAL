62f4c21e951cb177a8df113d683f10d0724fddf2
Uday <Uday@DESKTOP-QGBAF48>
2019-03-12 11:58:24 +0530

    Second changed to nd

--- AddTwoIntegers.java
+++ AddTwoIntegers.java
@@ -3,9 +3,9 @@ public class AddTwoIntegers {
     public static void main(String[] args) {
         
         int first = 10;
-        int second = 20;
+        int nd = 20;
 
-        int sum = first + second;
+        int sum = first + nd;
 
         System.out.println("The sum is: " + sum);
     }
